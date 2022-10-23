8f8c0bcf73b276d8a0ad2f2df968ec5f637a86b4
paolopaolo383 <481926paolo@gmail.com>
2022-10-24 00:40:37 +0900

    ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
    ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ

--- src/main/java/appearence/appearence/Appearence.java
+++ src/main/java/appearence/appearence/Appearence.java
@@ -1,33 +1,23 @@
 package appearence.appearence;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Slime;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.*;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -35,7 +25,6 @@ import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftInventory;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftInventory;
-
 
 
 import java.io.File;
@@ -47,14 +36,14 @@ import java.util.UUID;
 import static org.bukkit.inventory.ItemFlag.HIDE_ATTRIBUTES;
 
 public final class Appearence extends JavaPlugin implements Listener, CommandExecutor {
-    ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-
-
+    public int product = 13;
+    public int first = 29;
+    public int second = 33;
+    public String appearencenpcname = "[외형변경]사연";
     public List<Material> materials = Arrays.asList(Material.DIAMOND_AXE,Material.DIAMOND_HOE,Material.DIAMOND_PICKAXE,Material.DIAMOND_SWORD);
     //public List<String> materiallist =null;
     public HashMap<UUID, Inventory> inv = new HashMap<UUID, Inventory>();
     ConsoleCommandSender consol = Bukkit.getConsoleSender();
-
     @Override
     public void onEnable()
     {
@@ -74,119 +63,70 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
             //consol.sendMessage(materiallist.get(i));
             //materials.add(Material.valueOf(materiallist.get(i)));
         //}
-        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.USE_ENTITY) {
-            @Override
-            public void onPacketReceiving(PacketEvent e) {
-
-                Player p = e.getPlayer();
-                PacketContainer packet = e.getPacket();
-                int id = packet.getIntegers().read(0);
-
-                if (.tutorialnpc.getId() == id) {
-                    if (packet.getEntityUseActions().read(0) != EnumWrappers.EntityUseAction.ATTACK) {
-
-                        p.sendMessage("pog");
-
-                    }
-                }
 
 
-            }
-        });
-        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.USE_ENTITY) {
-            @Override
-            public void onPacketReceiving(PacketEvent e) {
-
-                Player p = e.getPlayer();
-                PacketContainer packet = e.getPacket();
-                int id = packet.getIntegers().read(0);
-
-                if (.tutorialnpc.getId() == id) {
-                    if (packet.getEntityUseActions().read(0) != EnumWrappers.EntityUseAction.ATTACK) {
-
-                        p.sendMessage("pog");
-
-                    }
-                }
-
-
-            }
-        });
     }
-
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args)
+    {
+        Player player = (Player) sender;
+        if(!sender.isOp())
+        {
+            return true;
+        }
+        if(player.getName().equalsIgnoreCase("Geune")&&command.getName().equalsIgnoreCase("외형변경권"))
+        {
+            ItemStack item = new ItemStack(Material.PAPER,1);
+            ItemMeta itemm = item.getItemMeta();
+            itemm.setLore(Arrays.asList(String.valueOf(player.getInventory().getItemInMainHand().getDurability()), String.valueOf(player.getInventory().getItemInMainHand().getType())));
+            item.setItemMeta(itemm);
+            player.getInventory().addItem(item);
+        }
+        if(command.getName().equalsIgnoreCase("외형"))
+        {
+            openInventory(player);
+        }
+        return true;
+    }
     @Override
     public void onDisable()
     {
 
     }
-    @EventHandler
-    public void onPlayerClickEvent(PlayerInteractEvent e)
-    {
-        Player player = e.getPlayer();
-        if(player.getInventory().getItemInMainHand()!=null)
-        {
-            if(e.getAction().equals(Action.RIGHT_CLICK_AIR))
-            {
-                if(player.getInventory().getItemInMainHand().getType()== Material.ANVIL)
-                {
-                    if(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase("외형변경권"))
-                    {
-                        openInventory(player);
-                    }
-                }
-            }
-            if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-            {
-                if(player.getInventory().getItemInMainHand().getType()== Material.ANVIL)
-                {
-                    if(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase("외형변경권"))
-                    {
-                        e.setCancelled(true);
-                        openInventory(player);
-                    }
-                }
-            }
-        }
-    }
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] args)
-    {
-        if(command.getName().equalsIgnoreCase("외형"))
-        {
-            //consol.sendMessage(ChatColor.AQUA+"[Hypixel] 월드 생성중");
-            Player player = (Player) sender;
-            player.getInventory().addItem(createGuiItem(Material.ANVIL,"외형변경권"));
-            ItemStack itemStack = createUIitem(Material.DIAMOND_PICKAXE,"외형으로 쓰일 무기(?)", (short)50,"t","h","i","s","is lore");
-            ItemStack item = createGuiItem(Material.DIAMOND_PICKAXE,"기능으로 쓰일 무기","무기임","암튼 무기임");
-            player.getInventory().addItem(item);
-            player.getInventory().addItem(itemStack);
-        }
-        return true;
-    }
+
     @EventHandler
     public void PlayerJoinEvent(PlayerJoinEvent e)
     {
-        inv.put(e.getPlayer().getUniqueId(),Bukkit.createInventory(null, 9, "combine"));
+        inv.put(e.getPlayer().getUniqueId(),Bukkit.createInventory(null, 54, "combine"));
         initializeItems(e.getPlayer().getUniqueId());
     }
 
 
     public void initializeItems(UUID uuid) {
         inv.get(uuid).clear();
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"input1"));///
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"input2"));///
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"output"));///
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
-        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3,"menu"));
+        ItemStack green = new ItemStack(Material.IRON_BLOCK,1);
+        ItemStack red = new ItemStack(Material.BARRIER,1);
+        for(int i = 0;i<product;i++)
+            inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)64));
+        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3));
+        for(int i = (product+1);i<first;i++)
+            inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)64));
+        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3));
+        for(int i = (first+1);i<second;i++)
+            inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)64));
+        inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)3));///
+        for(int i = (second+1);i<54;i++)
+            inv.get(uuid).addItem(createUIitem(Material.IRON_AXE, " ", (short)64));
 
-
+        inv.get(uuid).setItem(product+9,createUIitem(Material.IRON_AXE, " ", (short)2));
     }
 
 
+    @EventHandler
+    public void playerqevent(PlayerDropItemEvent e)
+    {
+
+    }
     protected ItemStack createUIitem(final Material material, final String name,final short damage, final String... lore) {
         final ItemStack item = new ItemStack(material, 1,damage);
         final ItemMeta meta = item.getItemMeta();
@@ -214,34 +154,58 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
 
         return item;
     }
-    public ItemStack combine(final ItemStack a,final ItemStack b) {
-        ItemStack item = new ItemStack(a.getType(), 1);
-        item.setDurability(a.getDurability());
-
-
-        item.setItemMeta(b.getItemMeta());
-        return item;
-    }
     public void openInventory(final HumanEntity ent) {
         initializeItems(ent.getUniqueId());
         ent.openInventory(inv.get(ent.getUniqueId()));
 
     }
-    public void swapitem(Inventory in)
+    public ItemStack combine(final ItemStack a,final ItemStack b) {
+        ItemStack item = new ItemStack(Material.valueOf(a.getItemMeta().getLore().get(1)), 1);
+        item.setDurability(Short.valueOf(a.getItemMeta().getLore().get(0)));
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(b.getItemMeta().getDisplayName());
+        meta.setLore(b.getItemMeta().getLore());
+        meta.addItemFlags(HIDE_ATTRIBUTES);
+        meta.setUnbreakable(true);
+        item.setItemMeta(meta);
+        return item;
+    }
+    public boolean swapitem(Inventory in)
     {
-        if(!in.getItem(2).getType().equals(Material.IRON_AXE)&&!in.getItem(4).getType().equals(Material.IRON_AXE))
+        if(in.getItem(first).getType().equals(Material.PAPER)&&!in.getItem(second).getType().equals(Material.IRON_AXE))
         {
-            if(in.getItem(2).getType().equals(in.getItem(4).getType()))
+            Material ap;
+            try
             {
-                if(materials.contains(in.getItem(2).getType()))
+                ap = Material.valueOf(in.getItem(first).getItemMeta().getLore().get(1));
+            }
+            catch (Exception e)
+            {
+                in.setItem(product,createUIitem(Material.IRON_AXE, " ", (short)3));
+                in.setItem(product+9,createUIitem(Material.IRON_AXE, " ", (short)2));
+                return false;
+            }
+            if(materials.contains(ap)&&materials.contains(in.getItem(second).getType()))
+            {
+                try
                 {
-                    in.setItem(6,combine(in.getItem(2),in.getItem(4)));
-                    return;
+                    in.setItem(product,combine(in.getItem(first),in.getItem(second)));
+                    in.setItem(product+9,createUIitem(Material.IRON_AXE, " ", (short)3));
+                    return true;
+
                 }
+                catch (Exception e)
+                {
+                    in.setItem(product,createUIitem(Material.IRON_AXE, " ", (short)3));
+                    in.setItem(product+9,createUIitem(Material.IRON_AXE, " ", (short)2));
+                    return false;
+                }
+
             }
         }
-        in.setItem(6,createUIitem(Material.IRON_AXE, " ", (short)3,"output"));
-
+        in.setItem(product,createUIitem(Material.IRON_AXE, " ", (short)3));
+        in.setItem(product+9,createUIitem(Material.IRON_AXE, " ", (short)2));
+        return false;
     }
 
     @EventHandler
@@ -251,14 +215,14 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
         Player p = (Player) e.getPlayer();
         if(!inv.get(p.getUniqueId()).getItem(8).getType().equals(Material.IRON_AXE))
             return;
-        if(!inv.get(p.getUniqueId()).getItem(2).getType().equals(Material.IRON_AXE))
+        if(!inv.get(p.getUniqueId()).getItem(first).getType().equals(Material.IRON_AXE))
         {
-            p.getInventory().addItem(inv.get(p.getUniqueId()).getItem(2));
+            p.getInventory().addItem(inv.get(p.getUniqueId()).getItem(first));
 
         }
-        if(!inv.get(p.getUniqueId()).getItem(4).getType().equals(Material.IRON_AXE))
+        if(!inv.get(p.getUniqueId()).getItem(second).getType().equals(Material.IRON_AXE))
         {
-            p.getInventory().addItem(inv.get(p.getUniqueId()).getItem(4));
+            p.getInventory().addItem(inv.get(p.getUniqueId()).getItem(second));
         }
         initializeItems(p.getUniqueId());
     }
@@ -285,12 +249,14 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
         }
 
         final ItemStack clickedItem = e.getCurrentItem();
+        ItemStack green = new ItemStack(Material.IRON_BLOCK,1);
+        ItemStack red = new ItemStack(Material.BARRIER,1);
 
-
-
+        Boolean a=false;
+        Boolean b = false;
         final Player p = (Player) e.getWhoClicked();
         ItemMeta itemmeta  =  e.getCurrentItem().getItemMeta();
-        if(e.getRawSlot()==2)
+        if(e.getRawSlot()==first)
         {
 
             if(e.getCurrentItem().getType().equals(Material.IRON_AXE))//공백
@@ -302,9 +268,12 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                 else
                 {
                     e.setCancelled(true);
-                    e.getInventory().setItem(2,e.getCursor());
+                    e.getInventory().setItem(first,e.getCursor());
                     e.setCursor(new ItemStack(Material.AIR));
-                    swapitem(e.getClickedInventory());
+
+                    a=swapitem(e.getClickedInventory());
+                    p.getWorld().playSound(p.getLocation(),Sound.ITEM_BOTTLE_FILL_DRAGONBREATH,2,0.5f);
+
                 }
             }
             else
@@ -313,7 +282,7 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                 {//아이템이 커서로 오고 인벤토리에 공백 아이템이 오게하고 스왑 아이템
                     e.setCancelled(true);
                     e.setCursor(e.getCurrentItem());
-                    e.getInventory().setItem(2,createUIitem(Material.IRON_AXE, " ", (short)3,"input1"));
+                    e.getInventory().setItem(first,createUIitem(Material.IRON_AXE, " ", (short)3));
                     swapitem(e.getClickedInventory());
                 }
                 else
@@ -322,18 +291,17 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                     ItemStack temp = e.getCursor();
 
                     e.setCursor(e.getCurrentItem());
-                    e.getInventory().setItem(2,temp);
-                    swapitem(e.getClickedInventory());
+                    e.getInventory().setItem(first,temp);
+                    a=swapitem(e.getClickedInventory());
+                    p.getWorld().playSound(p.getLocation(),Sound.ITEM_BOTTLE_FILL_DRAGONBREATH,2,0.5f);
                 }
             }
-
-
 
 
             //e.getInventory().setItem(6,combine(e.getInventory().getItem(2),e.getInventory().getItem(4)));
 
         }
-        else if(e.getRawSlot()==4)
+        else if(e.getRawSlot()==second)
         {
 
             if(e.getCurrentItem().getType().equals(Material.IRON_AXE))//공백
@@ -345,9 +313,11 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                 else
                 {
                     e.setCancelled(true);
-                    e.getInventory().setItem(4,e.getCursor());
+                    e.getInventory().setItem(second,e.getCursor());
                     e.setCursor(new ItemStack(Material.AIR));
-                    swapitem(e.getClickedInventory());
+                    b=swapitem(e.getClickedInventory());
+                    p.getWorld().playSound(p.getLocation(),Sound.ITEM_BOTTLE_FILL_DRAGONBREATH,2,0.5f);
+
                 }
             }
             else
@@ -356,7 +326,7 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                 {
                     e.setCancelled(true);
                     e.setCursor(e.getCurrentItem());
-                    e.getInventory().setItem(4,createUIitem(Material.IRON_AXE, " ", (short)3,"input2"));
+                    e.getInventory().setItem(second,createUIitem(Material.IRON_AXE, " ", (short)3));
                     swapitem(e.getClickedInventory());
                 }
                 else
@@ -365,28 +335,31 @@ public final class Appearence extends JavaPlugin implements Listener, CommandExe
                     ItemStack temp = e.getCursor();
 
                     e.setCursor(e.getCurrentItem());
-                    e.getInventory().setItem(4,temp);
-                    swapitem(e.getClickedInventory());
+                    e.getInventory().setItem(second,temp);
+                    b=swapitem(e.getClickedInventory());
+                    p.getWorld().playSound(p.getLocation(),Sound.ITEM_BOTTLE_FILL_DRAGONBREATH,2,0.5f);
                 }
             }
-
         }
-        else if(e.getRawSlot()==6&&!e.getCurrentItem().getType().equals(Material.IRON_AXE))
+        else if(e.getRawSlot()==product&&!e.getCurrentItem().getType().equals(Material.IRON_AXE))
         {
 
-            p.getInventory().addItem(e.getClickedInventory().getItem(6));
+            p.getInventory().addItem(e.getClickedInventory().getItem(product));
 
-            p.getInventory().removeItem(createGuiItem(Material.ANVIL,"외형변경권"));
             //외형변경권 없애기
             initializeItems(p.getUniqueId());
             p.closeInventory();
+            p.getWorld().playSound(p.getLocation(),Sound.BLOCK_ANVIL_USE,2,0.5f);
+
+
+            p.sendMessage(ChatColor.DARK_GRAY+ "외형이 변경되었습니다.");
 
         }
         else
         {
             e.setCancelled(true);
         }
-        //2 4 6
+
 
     }
 
